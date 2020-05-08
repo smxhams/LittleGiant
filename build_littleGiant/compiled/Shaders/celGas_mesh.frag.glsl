5a5fbd40643ba7eea82dedaf9559ca33c4a67e0a
smxhams <54346071+smxhams@users.noreply.github.com>
2020-05-08 18:47:24 +1200

    click drag mass redirect

---
+++ build_littleGiant/compiled/Shaders/celGas_mesh.frag.glsl
@@ -0,0 +1,28 @@
+#version 450
+#include "compiled.inc"
+#include "std/gbuffer.glsl"
+in vec2 texCoord;
+in vec3 wnormal;
+out vec4 fragColor[2];
+uniform sampler2D ImageTexture;
+void main() {
+vec3 n = normalize(wnormal);
+	vec4 ImageTexture_texread_store = texture(ImageTexture, texCoord.xy);
+	ImageTexture_texread_store.rgb = pow(ImageTexture_texread_store.rgb, vec3(2.2));
+	vec3 basecol;
+	float roughness;
+	float metallic;
+	float occlusion;
+	float specular;
+	vec3 ImageTexture_Color_res = ImageTexture_texread_store.rgb;
+	basecol = ImageTexture_Color_res;
+	roughness = 0.0;
+	metallic = 0.0;
+	occlusion = 1.0;
+	specular = 0.0;
+	n /= (abs(n.x) + abs(n.y) + abs(n.z));
+	n.xy = n.z >= 0.0 ? n.xy : octahedronWrap(n.xy);
+	const uint matid = 0;
+	fragColor[0] = vec4(n.xy, roughness, packFloatInt16(metallic, matid));
+	fragColor[1] = vec4(basecol, packFloat2(occlusion, specular));
+}
