10dfabf5a03d961f9b7dbd3e755a8964fce398c5
smxhams <54346071+smxhams@users.noreply.github.com>
2020-05-12 20:22:57 +1200

    Blue hex rings

--- Sources/arm/MainGame.hx
+++ Sources/arm/MainGame.hx
@@ -56,7 +56,7 @@ class MainGame extends iron.Trait {
 			if (canvas.getElement('contHexValues').visible == true) hexValuePos();
 
 			// Clicking and dragging to direct flow
-			if (mouse.started('left') && lastHover != null) {
+			if (mouse.started('left') && lastHover != null && lastHover != InitGame.inst.homeIndex) {
 				clickStart = lastHover;
 				iron.Scene.active.spawnObject('contArrow', null, function(o:Object) {
 					o.transform.loc.x = data[clickStart][0].o.transform.loc.x;
@@ -67,7 +67,7 @@ class MainGame extends iron.Trait {
 				});
 				
 			}
-			else if (mouse.down('left') && lastHover != null) {
+			else if (mouse.down('left') && lastHover != null && clickStart != null) {
 				if (data[clickStart][0].n.indexOf(lastHover) != -1 && lastHover != null) {
 					if (tempObj.children[0]!=null) tempObj.children[0].visible = true;
 					if (data[clickStart][0].outO != null) data[clickStart][0].outO.children[0].visible = false;
@@ -83,11 +83,65 @@ class MainGame extends iron.Trait {
 				}
 				//trace(mouse.x + " x " + mouse.y);
 			}
-			else if (mouse.released('left') && lastHover != null) {
-				if (data[clickStart][0].n.indexOf(lastHover) != -1 && lastHover != null) {
+			else if (mouse.released('left') && lastHover != null && clickStart != null) {
+				if (data[clickStart][0].n.indexOf(lastHover) != -1 && lastHover != null) { //If neighbor, create link
+					if (data[clickStart][0].outO != null) { // If reassinging a pre-existing link
+						data[clickStart][0].outO.remove(); //Remove link
+						var r:Array<Int> = data[data[clickStart][0].out][0].inI;
+						r.remove(clickStart);
+						data[data[clickStart][0].out][0].inI = r; //Removes input on old output
+						checkRings(lastHover); // Will take away blue rings if no longer leading home
+					}
 					data[clickStart][0].out = lastHover;
-					if (data[clickStart][0].outO != null) data[clickStart][0].outO.remove();
 					data[clickStart][0].outO = tempObj;
+					data[lastHover][0].inI.push(data[clickStart][0].i);
+
+
+					// Check if route leads home.
+					var homeRouteCheck = false;
+					var out = data[clickStart][0].out;
+					var startOut = data[clickStart][0].out;
+					while (homeRouteCheck == false) {
+						if (out == null) {
+							homeRouteCheck = false;
+							trace('Does not lead home');
+							break;
+						}
+						
+						if (out == InitGame.inst.homeIndex) {
+							trace('Found Home');
+							var addRingsForward = true;
+							out = data[clickStart][0].i;
+							while (addRingsForward == true) {
+								if (data[out][0].i == InitGame.inst.homeIndex) {
+									addRingsForward = false;
+									break;
+								}
+								if (data[out][0].ringO == null || data[out][0].ringO.name != 'contHexBlue') {
+									iron.Scene.active.spawnObject('contHexBlue', null, function(o:Object) {
+										o.transform.loc.x = 1*(Math.sqrt(3)*data[out][0].x + Math.sqrt(3)/2*data[out][0].y);
+										o.transform.loc.y = 1*(3/2*data[out][0].y);
+										o.transform.loc.z = 0.0;
+										o.transform.buildMatrix();
+										InitGame.inst.hexTilesObjects.add(o);
+										o.properties['id'] = data[out][0].i;
+										data[out][0].ringO = o;
+									});
+								}
+								out = data[out][0].out;
+							}
+							addRingsBackward(data[clickStart][0].i);
+
+							homeRouteCheck = true;
+							break;
+						}
+						out = data[out][0].out;
+						if (out == startOut){
+							homeRouteCheck = false;
+							trace('Loop detected');
+							break;
+						}
+					}
 				}
 				else if (tempObj.children[0]!=null) tempObj.children[0].visible = false;
 				clickStart = null;
@@ -245,4 +299,39 @@ class MainGame extends iron.Trait {
 			}
 		}
 	}
+	function addRingsBackward(inHex:Int) {
+		if (data[inHex][0].inI != []) {
+			for (i in 0...data[inHex][0].inI.length) {
+				var input = data[inHex][0].inI[i];
+				if (data[input][0].ringO == null || data[input][0].ringO.name != 'contHexBlue') {
+					iron.Scene.active.spawnObject('contHexBlue', null, function(o:Object) {
+						o.transform.loc.x = 1*(Math.sqrt(3)*data[input][0].x + Math.sqrt(3)/2*data[input][0].y);
+						o.transform.loc.y = 1*(3/2*data[input][0].y);
+						o.transform.loc.z = 0.0;
+						o.transform.buildMatrix();
+						InitGame.inst.hexTilesObjects.add(o);
+						o.properties['id'] = data[input][0].i;
+						data[input][0].ringO = o;
+					});
+				}
+				if (data[input][0].inI != []) addRingsBackward(data[input][0].i);
+			}
+			trace(data[inHex][0].inI);
+		}
+	}
+
+	function checkRings(hex:Int) {
+		if (data[hex][0].ringO != null) {
+			data[hex][0].ringO.remove();
+			data[hex][0].ringO = null;
+		}
+		if (data[hex][0].inI != []) {
+			for (i in 0...data[hex][0].inI.length) {
+				var input = data[hex][0].inI[i];
+				if (data[input][0].inI != []) checkRings(data[input][0].i);
+			}
+			trace(data[hex][0].inI);
+		}
+	}
+
 }
---
+++ build_littleGiant/compiled/Shaders/HexBlue_mesh.frag.glsl
@@ -0,0 +1,23 @@
+#version 450
+#include "compiled.inc"
+#include "std/gbuffer.glsl"
+in vec3 wnormal;
+out vec4 fragColor[2];
+void main() {
+vec3 n = normalize(wnormal);
+	vec3 basecol;
+	float roughness;
+	float metallic;
+	float occlusion;
+	float specular;
+	basecol = vec3(0.0, 0.05742466077208519, 0.15743215382099152);
+	roughness = 1.0;
+	metallic = 0.0;
+	occlusion = 1.0;
+	specular = 0.0;
+	n /= (abs(n.x) + abs(n.y) + abs(n.z));
+	n.xy = n.z >= 0.0 ? n.xy : octahedronWrap(n.xy);
+	const uint matid = 0;
+	fragColor[0] = vec4(n.xy, roughness, packFloatInt16(metallic, matid));
+	fragColor[1] = vec4(basecol, packFloat2(occlusion, specular));
+}
